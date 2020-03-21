8186db65bc6b8999de2009737010f16b7fbfd232
Mahmoud <mahmoudessam24@gmail.com>
2020-03-21 13:04:24 +0300

    Creating project

---
+++ src/main/java/sa/com/futureworkshops/test/articles/DemoApplication.java
@@ -0,0 +1,13 @@
+package sa.com.futureworkshops.test.articles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sa/com/futureworkshops/test/articles/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.com.futureworkshops.test.articles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
